bbb91a2339d32acbd308d7a9c905bbb6bbbef54e
netak <netak@wix.com>
2020-08-27 17:58:19 +0300

    build deploy hook

--- server/index.js
+++ server/index.js
@@ -9,8 +9,15 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+app.get('/.well-known/pki-validation', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/public', '883748C0D3374101C0E8A2C788C7F86E.txt'));
+});
+
+app.get('/api/*', (req, res) => {
+    res.send('Hello World!')
 });
 
 app.listen(port, () => {
