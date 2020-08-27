058116a361aa54444cac1abd9dacea0c7069298f
netak <netak@wix.com>
2020-08-27 17:58:19 +0300

    build deploy hook

--- server/index.js
+++ server/index.js
@@ -9,8 +9,11 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+app.get('/api/*', (req, res) => {
+    res.send('Hello World!')
 });
 
 app.listen(port, () => {
