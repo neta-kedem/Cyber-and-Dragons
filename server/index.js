282f7e092b09510725806a3ded90efc87b02f086
Neta Kedem <neta.ked@gmail.com>
2020-11-11 08:53:08 +0200

    will you work now?

--- server/index.js
+++ server/index.js
@@ -1,25 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 3003;
-const path = require('path');
+const path = require("path");
 //
 // app.get('/', (req, res) => {
 //     res.send('Hello World!')
 // })
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
-app.get('/.well-known/pki-validation', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/public', '883748C0D3374101C0E8A2C788C7F86E.txt'));
+app.get("/api/*", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/api/*', (req, res) => {
-    res.send('Hello World!')
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(port, () => {
-    console.log(`listening at port ${port}`)
-})
+  console.log(`listening at port ${port}`);
+});
