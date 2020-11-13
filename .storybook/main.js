100d28c33618006ae92c47c6e53f5ca8e1a626bc
Neta Kedem <neta.ked@gmail.com>
2020-11-13 06:18:30 +0200

    add chromatic script

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../client/src/**/*.stories.mdx",
+    "../client/src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ]
+}
