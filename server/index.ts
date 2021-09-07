29c830845ac616e71522272c3c32d38ff42e8bb1
NetaK <neta@getbalance.com>
2021-09-07 16:33:55 +0300

    try heroku build 2

--- server/index.ts
+++ server/index.ts
@@ -4,12 +4,6 @@ import express from "express"
 const app = express()
 const port = process.env.PORT || 3003
 import path from "path"
-const {env} = process
-
-//
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
