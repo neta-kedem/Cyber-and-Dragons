e0692bcd7c4346b4f765ee9940bec57db7f58dac
NetaK <neta@getbalance.com>
2021-09-07 16:47:58 +0300

    try heroku build 3

--- server/index.ts
+++ server/index.ts
@@ -5,14 +5,14 @@ const app = express()
 const port = process.env.PORT || 3003
 import path from "path"
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "../../client/build")));
 
 app.get("/api/*", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
 });
 
 app.listen(port, () => {
